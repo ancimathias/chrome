c8d1b3e8b29260e732984b3801f04b88d84d6f10
Administrator <Administrator@AncitaMathias27>
2019-01-28 12:09:13 +0530

    commit 1

---
+++ Magentomaven/src/test/java/LaunchTest.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LaunchTest {
+	@Test
+	public void launchTest() {
+		
+		
+		 ChromeDriver driver = new ChromeDriver();
+		
+	}
+
+}
